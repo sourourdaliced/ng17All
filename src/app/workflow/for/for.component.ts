97c3811bdaeac10faae268c4317be879eaf3652a
aymensellaouti <aymen.sellaouti@gmail.com>
2024-03-12 12:32:00 +0100

    add empty bloc and automatic control flow migration script

--- src/app/workflow/for/for.component.ts
+++ src/app/workflow/for/for.component.ts
@@ -9,6 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './for.component.css',
 })
 export class ForComponent {
+  players: { id: number, name: string }[] = [];
+  constructor() {
+    setTimeout(() => {
+      this.players = [{ name: "sellaouti", id: 1 }, { name: "zidane", id:2 }, { name: "messi", id: 3 }];
+    }, 1500);
+  }
   episodes = [
     { id: 1, title: 'Episode 1' },
     { id: 2, title: 'Episode 2' },
